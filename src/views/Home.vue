89b2f313ceb314f814a153efdb4cc80ab4f96dab
Matheus Osterno <matheusosternodev@gmail.com>
2022-11-28 22:28:06 -0300

    Section APP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+
   </div>
 </template>
 
@@ -8,5 +8,28 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost:
+          "Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        welcomeScreen:true,
+        photo:"Coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "this is a Filter Title!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "beatiful-stories",
+        },
+        {
+          title: "this is a Filter Title!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "designed-for-everyone",
+        }
+      ],
+    }
+  },
 };
 </script>
